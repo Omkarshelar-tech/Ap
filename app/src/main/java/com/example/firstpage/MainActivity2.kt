6727a6c1ea47c1fb3d1dc987e7a6365e855597d4
Omkarshelar-tech <shelaromkar492@gmail.com>
2025-01-16 16:14:45 +0530

    commit changes

---
+++ app/src/main/java/com/example/firstpage/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.firstpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstpage.ui.main.MainFragment
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.rview)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
